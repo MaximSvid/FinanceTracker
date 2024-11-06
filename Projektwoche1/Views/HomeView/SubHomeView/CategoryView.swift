3e09deb1f23dc97f726ca7f238dbc38e6aa0d6e0
Maxim Svidrak <maximsvid@gmail.com>
2024-11-06 14:07:04 +0100

    scroll categoryView

--- Projektwoche1/Views/HomeView/SubHomeView/CategoryView.swift
+++ Projektwoche1/Views/HomeView/SubHomeView/CategoryView.swift
@@ -10,28 +10,7 @@ import SwiftUI
 struct CategoryView: View {
     
     
-//    @State var  categories: [Category] = [
-//        Category(id: UUID(), name: "Food", icon: "fork.knife"),
-//        Category(id: UUID(), name: "Shopping", icon: "cart.fill"),
-//        Category(id: UUID(), name: "Transport", icon: "car.fill"),
-//        Category(id: UUID(), name: "Bicycle", icon: "bicycle"),
-//        Category(id: UUID(), name: "Health", icon: "heart.fill"),
-//        Category(id: UUID(), name: "Bills", icon: "doc.text"),
-//        Category(id: UUID(), name: "Entertainment", icon: "gamecontroller.fill"),
-//        Category(id: UUID(), name: "Education", icon: "book.fill"),
-//        Category(id: UUID(), name: "Travel", icon: "airplane"),
-//        Category(id: UUID(), name: "Gifts", icon: "gift.fill"),
-//        Category(id: UUID(), name: "Home", icon: "house.fill"),
-//        Category(id: UUID(), name: "Pet", icon: "pawprint.fill"),
-//        Category(id: UUID(), name: "Savings", icon: "banknote.fill"),
-//        Category(id: UUID(), name: "Insurance", icon: "shield.fill"),
-//        Category(id: UUID(), name: "Investment", icon: "chart.line.uptrend.xyaxis"),
-//        Category(id: UUID(), name: "Family", icon: "person.3.fill"),
-//        Category(id: UUID(), name: "Work", icon: "briefcase.fill"),
-//        Category(id: UUID(), name: "Phone", icon: "phone.fill"),
-//        Category(id: UUID(), name: "Utilities", icon: "bolt.fill"),
-//        Category(id: UUID(), name: "Miscellaneous", icon: "questionmark.circle.fill")
-//    ]
+
     
     var wallets: [Wallet]
     @Binding var categories: [Category]
@@ -48,11 +27,13 @@ struct CategoryView: View {
     
     var body: some View {
         
+        
+        ScrollView {
             LazyVGrid(columns: columns, spacing: 24) {
                 ForEach(categories, id: \.self) { category in
                     Circle()
                         .fill(Color.blue)
-                        .frame(width: 60, height: 60)    
+                        .frame(width: 60, height: 60)
                         .overlay(
                             Image(systemName: category.icon)
                                 .resizable()
@@ -68,13 +49,15 @@ struct CategoryView: View {
                 }
                 
             }
-            .padding(20) 
+            .padding(20)
             .sheet(item: $selectedCategory) { category in
                 HomeSheetView(wallets: wallets, isHomeSheetPresented: $isHomeSheetOpen, categoryName: category.name, categoryImage: category.icon, selectedCategory: $selectedCategory)
                         .presentationDetents([.fraction(0.6)])
                         .presentationDragIndicator(.visible)
                 }
-            
+
+        }
+                        
             
     }
     
