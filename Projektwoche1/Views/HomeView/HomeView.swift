c708826d45ce93aed8cda19360566d078b5a77f8
Maxim Svidrak <maximsvid@gmail.com>
2024-11-08 10:38:53 +0100

    init...

--- Projektwoche1/Views/HomeView/HomeView.swift
+++ Projektwoche1/Views/HomeView/HomeView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import SwiftData
 
 struct HomeView: View {
+    @Environment(\.modelContext) private var modelContext
+    @State private var isInitialized = false
     @Query private var wallets: [Wallet]
     @State private var isSheetDeleteCategoryPresented: Bool = false
     @State private var categories: [Category] = [
@@ -45,7 +47,29 @@ struct HomeView: View {
             
             
         }
+        .onAppear {
+            initializeData()
+        }
     }
+    
+    private func initializeData() {
+            if !isInitialized {
+                let wallet1 = Wallet(name: "Wallet 1", image: "wallet8", balance: 3000)
+                let wallet2 = Wallet(name: "Wallet 2", image: "wallet5", balance: 4000)
+                let wallet3 = Wallet(name: "Wallet 3", image: "wallet3", balance: 2500)
+
+                modelContext.insert(wallet1)
+                modelContext.insert(wallet2)
+                modelContext.insert(wallet3)
+
+                do {
+                    try modelContext.save()
+                    isInitialized = true
+                } catch {
+                    print("Failed to save initial data: \(error)")
+                }
+            }
+        }
 }
 
 #Preview {
--- Projektwoche1/Views/WalletView/WalletView.swift
+++ Projektwoche1/Views/WalletView/WalletView.swift
@@ -8,41 +8,21 @@
 import SwiftUI
 
 struct WalletView: View {
-    @Environment(\.modelContext) private var modelContext
-    @State private var isInitialized = false
+    
     
     var body: some View {
         NavigationStack {
             VStack {
                 AddNewWallet()
                 ListWalletView()
-                    .onAppear {
-                        initializeData()
-                    }
+                    
             }
 
         }
        
     }
     
-    private func initializeData() {
-            if !isInitialized {
-                let wallet1 = Wallet(name: "Wallet 1", image: "wallet8", balance: 3000)
-                let wallet2 = Wallet(name: "Wallet 2", image: "wallet5", balance: 4000)
-                let wallet3 = Wallet(name: "Wallet 3", image: "wallet3", balance: 2500)
-
-                modelContext.insert(wallet1)
-                modelContext.insert(wallet2)
-                modelContext.insert(wallet3)
-
-                do {
-                    try modelContext.save()
-                    isInitialized = true
-                } catch {
-                    print("Failed to save initial data: \(error)")
-                }
-            }
-        }
+   
 }
 
 #Preview {
